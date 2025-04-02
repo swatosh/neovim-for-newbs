0857d422e445879133326716d4907f10e35fe66b
Joe Swatosh <jswatosh@bspot.com>
2025-04-02 16:35:20 -0700

    remove duplicate

--- lua/plugins/nvim-tmux-navigator.lua
+++
@@ -1,7 +0,0 @@
-return {
-  'christoomey/vim-tmux-navigator',
-  vim.keymap.set('n', 'C-h', ':TMuxNavigateLeft<CR>'),
-  vim.keymap.set('n', 'C-j', ':TMuxNavigateDown<CR>'),
-  vim.keymap.set('n', 'C-k', ':TMuxNavigateUp<CR>'),
-  vim.keymap.set('n', 'C-l', ':TMuxNavigateRight<CR>'),
-}
