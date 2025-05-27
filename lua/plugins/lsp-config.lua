43da46976e9554b34ac5181dfb22fd5dd0eafe3b
Joe Swatosh <jswatosh@bspot.com>
2025-05-27 13:37:36 -0700

    Move rubocop config from none to lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -29,6 +29,10 @@ return {
         capabilities = capabilities
       })
 
+      lspconfig.rubocop.setup({
+        capabilities = capabilities
+      })
+
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
