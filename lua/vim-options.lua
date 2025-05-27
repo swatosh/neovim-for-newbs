6ea78b22fa66db4f6cd6dda7e024dbf16b3c5017
Joe Swatosh <jswatosh@bspot.com>
2025-05-27 13:35:15 -0700

    highlight 120th column

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,6 +2,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("set colorcolumn=120")
 vim.g.mapleader = " "
 vim.g.background = "light"
 
